a6e3a30565c33d4dec5f61a4f3638f5570c08d87
Zakariyya Mughal <zaki.mughal@gmail.com>
2013-09-29 23:43:23 -0500

    add keywords
    This is just creating placeholders for all the MATLAB keywords.

--- lib/MarpaX/MATLAB.pm
+++ lib/MarpaX/MATLAB.pm
@@ -1,7 +1,5 @@
 package MarpaX::MATLAB;
 
-
-use Marpa::R2;
 use strict;
 use warnings;
 
@@ -10,9 +8,10 @@ use Marpa::R2;
 sub grammar {
 my $grammar = Marpa::R2::Scanless::G->new(
     {   action_object  => 'My_Nodes',
-        #default_action => '::first',
+	#default_action => '::first',
 	default_action => '::array',
-        source         => \(<<'END_OF_SOURCE'),
+	source         => \(<<'END_OF_SOURCE'),
+
 :start ::= Top
 
 Top ::=
@@ -23,12 +22,49 @@ Script_file ::= Statement+
 
 Function_file ::= Statement+
 
-Statement ::= Expression Semicolon
-            | Expression Newline
-            | Expression
+Statement ::=
+	  Expression Semicolon
+	| Expression Newline
+	| Expression
+	| Keyword
 
+Keyword ::=
+	  kw_For
+	| kw_End
+	| kw_If
+	| kw_While
+	| kw_Function
+	| kw_Return
+	| kw_Elseif
+	| kw_Case
+	| kw_Otherwise
+	| kw_Switch
+	| kw_Continue
+	| kw_Else
+	| kw_Try
+	| kw_Catch
+	| kw_Global
+	| kw_Persistent
+	| kw_Break
 
-kw_For ~ "for"
+kw_For        ~ 'for'
+kw_End        ~ 'end'
+kw_If         ~ 'if'
+kw_While      ~ 'while'
+kw_Function   ~ 'function'
+kw_Return     ~ 'return'
+kw_Elseif     ~ 'elseif'
+kw_Case       ~ 'case'
+kw_Otherwise  ~ 'otherwise'
+kw_Switch     ~ 'switch'
+kw_Continue   ~ 'continue'
+kw_Else       ~ 'else'
+kw_Try        ~ 'try'
+kw_Catch      ~ 'catch'
+kw_Global     ~ 'global'
+kw_Persistent ~ 'persistent'
+kw_Break      ~ 'break'
+
 
 Expression ::=
 	   Number
